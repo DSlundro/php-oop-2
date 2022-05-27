738925c17ee458372c163da39bb1bd77e619e00a
DSlundro <dragan.savic95@outlook.it>
2022-05-27 14:53:28 +0200

    add discount

--- Models/user.php
+++ Models/user.php
@@ -7,14 +7,22 @@ class User{
     protected $username;
     protected $email;
     protected $card;
+    protected $membership;
+    protected $discount;
 
-    public function __construct($name, $surname, $username, $email, $card)
+    public function __construct($name, $surname, $username, $email, $card, $membership, $discount = 0)
     {
         $this -> name = $name;
         $this -> surname = $surname;
         $this -> username = $username;
         $this -> email = $email;
         $this -> card = $card;
+        $this -> membership = $membership;
+        $this -> discount = $discount;
     }
 
+    public function getDiscount()
+    {
+        return ($this -> membership == 'premium') ? $this -> discount = 'Hai 20% di sconto' : 'Registrati per avere lo sconto!';
+    }
 }
--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 require __DIR__ . '/Models/products.php';
 require __DIR__ . '/Models/user.php';
 
-
-$newUser = new User('Mario', 'Rossi', 'MR69', 'mario.rossi@live.it', '1234-4321-5744-8657');
+// utente registrato
+$newUser = new User('Mario', 'Rossi', 'MR69', 'mario.rossi@live.it', '1234-4321-5744-8657', 'premium');
 var_dump($newUser);
+var_dump($newUser -> getDiscount());
+
+// utente non registrato
+$newUser1 = new User('Mario', 'Rossi', 'MR69', 'mario.rossi@live.it', '1234-4321-5744-8657', 'base');
+var_dump($newUser1);
+var_dump($newUser1 -> getDiscount());
 
 $newToy = new Toys ('Solid Rubber Ball', '$4.99', '299 pz', 'Lorem ipsum dolor sit amet.', 'Ball', 'Dogs', '1 year +', 'none');
 var_dump($newToy);
